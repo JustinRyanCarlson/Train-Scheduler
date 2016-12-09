bdd44d395ecf35ca4cd99d1d48441e15b6c3e3c9
Justin Carlson <justin.ryan.carlson@gmail.com>
2016-12-08 19:52:24 -0600

    changes the trigger to write new data from on value to on child added, completes the moment.js calculations for next arrival and minutes away, adds style to style.css, corrects script order in index.html, removes storing of keys from pushes to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,15 +9,73 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
-var pushKey;
+var pageLoadTrigger = false;
 var trainName;
 var destination;
 var firstTrainTime;
 var frenquency;
 
-setInterval(calcTrainTimes(), 60000);
-calcTrainTimes();
-displayTrains();
+
+
+database.ref().on("child_added", function(childSnapshot) {
+    if (pageLoadTrigger === false) {
+        pageLoadTrigger = true;
+
+        var rowDiv = $('<div>').addClass('row');
+        var trainNameCol = $('<div>').addClass('col-sm-3');
+        trainNameCol.append($('<h4>').addClass('margins').text('Train Name'));
+        rowDiv.append(trainNameCol);
+        var destinationCol = $('<div>').addClass('col-sm-3');
+        destinationCol.append($('<h4>').addClass('margins').text('Destination'));
+        rowDiv.append(destinationCol);
+        var frenquencyCol = $('<div>').addClass('col-sm-2 text-center');
+        frenquencyCol.append($('<h4>').addClass('margins').text('Frequency (min)'));
+        rowDiv.append(frenquencyCol);
+        var nextArrivalCol = $('<div>').addClass('col-sm-2 text-center');
+        nextArrivalCol.append($('<h4>').addClass('margins').text('Next Arrival'));
+        rowDiv.append(nextArrivalCol);
+        var minutesAwayCol = $('<div>').addClass('col-sm-2 text-center');
+        minutesAwayCol.append($('<h4>').addClass('margins').text('Minutes Away'));
+        rowDiv.append(minutesAwayCol);
+        $('#train-results').append(rowDiv);
+    }
+
+    var trainNameSnap = childSnapshot.val().trainName;
+    var destinationSnap = childSnapshot.val().destination;
+    var firstTrainTimeSnap = childSnapshot.val().firstTrainTime;
+    var firstTrainTimeSnapConv = moment(firstTrainTimeSnap, "HH:mm").subtract(1, "days");
+    var frenquencySnap = childSnapshot.val().frenquency;
+
+    var result = moment().diff(moment(firstTrainTimeSnapConv), "minutes");
+    var remainder = result % frenquencySnap;
+    var minUntilTrain = frenquencySnap - remainder;
+    var nextTrain = moment().add(minUntilTrain, 'minutes').format('LT');
+
+
+    $('#train-results').append('<hr>');
+    var rowDivFire = $('<div>').addClass('row');
+    var trainNameColFire = $('<div>').addClass('col-sm-3');
+    trainNameColFire.append($('<h5>').text(trainNameSnap));
+    rowDivFire.append(trainNameColFire);
+    var destinationColFire = $('<div>').addClass('col-sm-3');
+    destinationColFire.append($('<h5>').text(destinationSnap));
+    rowDivFire.append(destinationColFire);
+    var frenquencyColFire = $('<div>').addClass('col-sm-2 text-center');
+    frenquencyColFire.append($('<h5>').text(frenquencySnap));
+    rowDivFire.append(frenquencyColFire);
+    var nextArrivalColFire = $('<div>').addClass('col-sm-2 text-center');
+    nextArrivalColFire.append($('<h5>').text(nextTrain));
+    rowDivFire.append(nextArrivalColFire);
+    var minutesAwayColFire = $('<div>').addClass('col-sm-2 text-center');
+    minutesAwayColFire.append($('<h5>').text(minUntilTrain));
+    rowDivFire.append(minutesAwayColFire);
+
+    $('#train-results').append(rowDivFire);
+}, function(errorObject) {
+    // Log a read error and its error code.
+    console.log("The read failed: " + errorObject.code);
+});
+
 
 
 $('#submit-button').on('click', function() {
@@ -27,77 +85,17 @@ $('#submit-button').on('click', function() {
     frenquency = $('#frenquency').val().trim();
     $('#train-name, #destination, #first-train-time, #frenquency').val('');
 
+
     if (trainName === '' || destination === '' || firstTrainTime === '' || frenquency === '') {
         alert('Please fill out all fields');
     } else {
-        pushKey = database.ref().push({
+        database.ref().push({
             trainName: trainName,
             destination: destination,
             firstTrainTime: firstTrainTime,
-            key: 'none',
             frenquency: frenquency
-        }).key;
-        //not sure if i need the key, probabaly not
-        database.ref().child(pushKey).update({
-            key: pushKey
         });
     }
-    displayTrains();
+
     return false;
 });
-
-
-function displayTrains() {
-    $('#train-results').empty();
-    var rowDiv = $('<div>').addClass('row');
-    var trainNameCol = $('<div>').addClass('col-sm-3');
-    trainNameCol.append($('<h5>').text('Train Name'));
-    rowDiv.append(trainNameCol);
-    var destinationCol = $('<div>').addClass('col-sm-3');
-    destinationCol.append($('<h5>').text('Destination'));
-    rowDiv.append(destinationCol);
-    var frenquencyCol = $('<div>').addClass('col-sm-2');
-    frenquencyCol.append($('<h5>').text('Frequency (min)'));
-    rowDiv.append(frenquencyCol);
-    var nextArrivalCol = $('<div>').addClass('col-sm-2');
-    nextArrivalCol.append($('<h5>').text('Next Arrival'));
-    rowDiv.append(nextArrivalCol);
-    var minutesAwayCol = $('<div>').addClass('col-sm-2');
-    minutesAwayCol.append($('<h5>').text('Minutes Away'));
-    rowDiv.append(minutesAwayCol);
-    $('#train-results').append(rowDiv);
-
-    database.ref().on("value", function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-            var trainNameSnap = childSnapshot.val().trainName;
-            var destinationSnap = childSnapshot.val().destination;
-            var firstTrainTimeSnap = childSnapshot.val().firstTrainTime;
-            var frenquencySnap = childSnapshot.val().frenquency;
-
-            $('#train-results').append('<hr>');
-            var rowDiv = $('<div>').addClass('row');
-            var trainNameCol = $('<div>').addClass('col-sm-3');
-            trainNameCol.append($('<h5>').text(trainNameSnap));
-            rowDiv.append(trainNameCol);
-            var destinationCol = $('<div>').addClass('col-sm-3');
-            destinationCol.append($('<h5>').text(destinationSnap));
-            rowDiv.append(destinationCol);
-            var frenquencyCol = $('<div>').addClass('col-sm-2');
-            frenquencyCol.append($('<h5>').text(frenquencySnap));
-            rowDiv.append(frenquencyCol);
-            var nextArrivalCol = $('<div>').addClass('col-sm-2');
-            var minutesAwayCol = $('<div>').addClass('col-sm-2');
-            $('#train-results').append(rowDiv);
-        });
-        // If it fails, cue error handling.
-    }, function(errorObject) {
-        // Log a read error and its error code.
-        console.log("The read failed: " + errorObject.code);
-    });
-
-}
-
-
-function calcTrainTimes() {
-
-}
